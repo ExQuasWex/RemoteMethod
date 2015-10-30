2393e6fd235359e3fee9a71da67d84b3739f1425
ExQuasWex <villerdex@gmail.com>
2015-10-31 00:00:30 +0800

    Family

--- src/clientModel/Family.java
+++ src/clientModel/Family.java
@@ -5,12 +5,12 @@ import java.io.Serializable;
 /**
  * Created by reiner on 10/19/2015.
  */
-public class family implements Serializable {
+public class Family implements Serializable {
 
     private familyInfo familyinfo;
     private FamilyPoverty familypoverty;
 
-    public family(familyInfo familyinfo, FamilyPoverty familypoverty) {
+    public Family(familyInfo familyinfo, FamilyPoverty familypoverty) {
         this.familyinfo = familyinfo;
         this.familypoverty = familypoverty;
     }
